0cd9c5958707f625323ef21aa167adc57c946797
Axel <axel.hammarberg@gmail.com>
2022-12-10 00:29:33 +0100

    Did day 9

---
+++ day_9/content/main.erl
@@ -0,0 +1,77 @@
+-module(main). 
+-export([start/0]). 
+	
+moveRope({_, Amount}, Acc) when Amount == 0 -> Acc;
+moveRope({Dir, Amount}, {Visited, Rope}) -> 
+	{NewRope, _} = lists:mapfoldl(fun({X, Y}, Prev) ->
+			case Prev of
+			first -> 
+				Head = case Dir of 
+					$R -> {X + 1, Y};
+					$L -> {X - 1, Y};
+					$U -> {X, Y + 1};
+					$D -> {X, Y - 1}
+				end,
+				{Head, Head};
+			{Px, Py} -> 
+				DeltaX = Px - X,
+				DeltaY = Py - Y,
+				Pos = if 
+					abs(DeltaX) == 2 -> 
+					{
+						X + DeltaX div 2,
+						case DeltaY of
+							0 -> Y;
+							1 -> Y + 1;
+							2 -> Y + 1;
+							-1 -> Y - 1;
+							-2 -> Y - 1
+						end
+					};
+					abs(DeltaY) == 2 -> 
+					{
+						case DeltaX of
+							0 -> X;
+							1 -> X + 1;
+							2 -> X + 1;
+							-1 -> X - 1;
+							-2 -> X - 1
+						end,
+						Y + DeltaY div 2
+					};
+					true -> 
+						{X, Y}
+				end, 
+				{Pos, Pos}
+			end
+		end,
+		first, Rope
+	),
+	moveRope({Dir, Amount - 1}, {sets:add_element(lists:last(NewRope), Visited), NewRope}).
+
+start() -> 
+	case file:read_file("../input/input9.txt") of
+		{ok, Input} -> 
+			Lines = lists:map(fun(Ln) -> 
+					{
+						lists:nth(1, Ln), 
+						element(1, string:to_integer(lists:sublist(Ln, 3, length(Ln))))
+					}
+				end, 
+				[binary_to_list(Line) || Line <- binary:split(Input, [<<"\n">>], [global]), Line /= <<>>]
+			),
+			{Short, _} = lists:foldl(
+				fun moveRope/2,
+				{sets:add_element({0, 0}, sets:new()), lists:duplicate(2, {0, 0})}, 
+				Lines
+			), 
+			{Long, _} = lists:foldl(
+				fun moveRope/2,
+				{sets:add_element({0, 0}, sets:new()), lists:duplicate(10, {0, 0})}, 
+				Lines
+			), 
+			
+			io:fwrite("~w~n", [sets:size(Short)]),
+			io:fwrite("~w~n", [sets:size(Long)]);
+		{error, Reason} -> io:fwrite("~w~n", [Reason])
+	end.
